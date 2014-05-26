c2149e019f8084cfe63ae30bde2cd314feebd78d
Charlesan <ccmaster58@gmail.com>
2014-05-26 20:14:58 +0800

    First Commit.

---
+++ src/com/pikachu/activity/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.pikachu.activity;
+
+import com.pikachu.res.R;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends Activity {
+
+	// 帐号和密码
+	private EditText edname;
+	private EditText edpassword;
+
+	private Button btregister;
+	private Button btlogin;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+
+		edname = (EditText) findViewById(R.id.edname);
+		edpassword = (EditText) findViewById(R.id.edpassword);
+		btregister = (Button) findViewById(R.id.btregister);
+		btlogin = (Button) findViewById(R.id.btlogin);
+
+		// 跳转到注册界面
+		btregister.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(LoginActivity.this, RegistersActivity.class);
+				startActivity(intent);
+			}
+		});
+		btlogin.setOnClickListener(new LoginListener());
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+	
+	class LoginListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			String name = edname.getText().toString();
+			String password = edpassword.getText().toString();
+			if (name.equals("") || password.equals("")) {
+				// 弹出消息框
+				new AlertDialog.Builder(LoginActivity.this).setTitle("错误")
+						.setMessage("帐号或密码不能空")
+						.setPositiveButton("确定", null).show();
+			} else {
+				//isUserinfo(name, password);
+				Intent intent = new Intent();
+				intent.setClass(LoginActivity.this, SlidingActivity.class);
+				startActivity(intent);
+			}
+		}
+
+	}
+}
---
+++ src/com/pikachu/slidingmenu/fragment/LeftFragment.java
@@ -0,0 +1,137 @@
+package com.pikachu.slidingmenu.fragment;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.pikachu.res.R;
+
+public class LeftFragment extends Fragment implements OnClickListener {
+
+	private Button mainFragmentButton;
+	private Button radarButton;
+	private Button cameraButton;
+	private Button illustratedHandbookButton;
+	private Button myRankingButton;
+	private Button settingButton;
+	private Button exitButton;
+	
+	//用于记录当前被点击的侧边栏按钮
+	private Button currentButton;
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.left, null);
+
+		mainFragmentButton = (Button) view
+				.findViewById(R.id.main_fragment_button);
+		radarButton = (Button) view
+				.findViewById(R.id.radar_button);
+		cameraButton = (Button) view.findViewById(R.id.camera_button);
+		illustratedHandbookButton = (Button) view.findViewById(R.id.illustrated_handbook_button);
+		myRankingButton = (Button) view.findViewById(R.id.my_ranking_button);
+		settingButton = (Button) view.findViewById(R.id.setting_button);
+		exitButton = (Button) view.findViewById(R.id.exit_button);
+
+		mainFragmentButton.setBackgroundColor(Color.GRAY);
+		currentButton = mainFragmentButton;
+
+		return view;
+	}
+
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		mainFragmentButton.setOnClickListener(this);
+		radarButton.setOnClickListener(this);
+		cameraButton.setOnClickListener(this);
+		illustratedHandbookButton.setOnClickListener(this);
+		myRankingButton.setOnClickListener(this);
+		settingButton.setOnClickListener(this);
+		exitButton.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view) {
+		FragmentManager fm = getFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		switch (view.getId()) {
+
+		case R.id.main_fragment_button:
+			currentButton.setBackgroundColor(Color.TRANSPARENT);
+			currentButton = mainFragmentButton;
+			mainFragmentButton.setBackgroundColor(Color.GRAY);
+			ft.replace(R.id.center_frame, new RightFragment());
+			break;
+		case R.id.radar_button:
+			currentButton.setBackgroundColor(Color.TRANSPARENT);
+			currentButton = radarButton;
+			radarButton.setBackgroundColor(Color.GRAY);
+			ft.replace(R.id.center_frame, new RightFragment());
+			break;
+		case R.id.camera_button:
+			currentButton.setBackgroundColor(Color.TRANSPARENT);
+			currentButton = cameraButton;
+			cameraButton.setBackgroundColor(Color.GRAY);
+			ft.replace(R.id.center_frame, new RightFragment());
+			break;
+		case R.id.illustrated_handbook_button:
+			currentButton.setBackgroundColor(Color.TRANSPARENT);
+			currentButton = illustratedHandbookButton;
+			illustratedHandbookButton.setBackgroundColor(Color.GRAY);
+			ft.replace(R.id.center_frame, new RightFragment());
+			break;
+		case R.id.my_ranking_button:
+			currentButton.setBackgroundColor(Color.TRANSPARENT);
+			currentButton = myRankingButton;
+			myRankingButton.setBackgroundColor(Color.GRAY);
+			ft.replace(R.id.center_frame, new RightFragment());
+			break;
+		case R.id.setting_button:
+			currentButton.setBackgroundColor(Color.TRANSPARENT);
+			currentButton = settingButton;
+			settingButton.setBackgroundColor(Color.GRAY);
+			ft.replace(R.id.center_frame, new RightFragment());
+			break;
+		case R.id.exit_button:
+			currentButton.setBackgroundColor(Color.TRANSPARENT);
+			currentButton = exitButton;
+			exitButton.setBackgroundColor(Color.GRAY);
+			// 弹出消息框
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+			builder.setMessage("您确定要退出应用？")
+					.setCancelable(false)
+					.setPositiveButton("是",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int id) {
+									getActivity().finish();
+								}
+							})
+					.setNegativeButton("否",
+							new DialogInterface.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int id) {
+									dialog.cancel();
+								}
+							});
+			builder.show();
+			break;
+		default:
+			break;
+		}
+		// 不要忘记提交
+		ft.commit();
+
+	}
+
+}
